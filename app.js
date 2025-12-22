1fb8cf4b85bfd8eee1df6beee852abbe1177d290
franpi <frannpinero@hotmail.com>
2025-12-22 20:49:31 -0300

    Request y Response

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const http = require('http');
 
 http.createServer( (req, res) => {
 
-    res.write('Hola mundo!');
+
+    // res.writeHead(200,  { 'Content-Type': 'application/json' });
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200,  { 'Content-Type': 'application/csv' });
+
+    res.write( 'id, nombre\n' );
+    res.write( '1, Junior\n' );
+    res.write( '2, Mike\n' );
+    res.write( '3, Juan\n' );
+    res.write( '4, Pedro\n' );
     res.end();
 
 } )
