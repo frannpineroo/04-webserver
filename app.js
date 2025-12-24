d3314196a6abd8f2e5b62a9c43140bad00e981a7
franpi <frannpinero@hotmail.com>
2025-12-24 18:11:18 -0300

    servir un sitio web

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 
 const app = express()
 const port = 8080
@@ -10,8 +16,12 @@ app.get('/hola-mundo', (req, res) => {
   res.send('Hola mundo desde su respectiva ruta')
 })
 
+app.get('/usuario', (req, res) => {
+  res.sendFile( __dirname + '/public/usuario.html' )
+})
+
 app.get('/error', (req, res) => {
-  res.send('404 | Página no encontrada')
+  res.sendFile( __dirname + '/public/404.html' )
 })
 
 app.listen(8080, () => {
